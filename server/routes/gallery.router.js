e4ad8977fcfd99162d212d320d0a43b1b6b0245c
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-16 19:18:28 -0500

    Created pool.js and added pg and body-parser to modules

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,51 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data'); // OLD NON-DB ROUTE
+const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    let updateId = req.params.id;
+
+    const sqlText = `UPDATE "gallery"
+                        SET "likes" = "likes" + 1
+                        WHERE "id" = $1;`
+
+    pool.query(sqlText, [updateId])
+    .then((result) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('Error sending like', error);
+        res.sendStatus(500);
+    })
+
+    // OLD NON-DB ROUTE
+    // const galleryId = req.params.id;
+    // for(const galleryItem of galleryItems) {
+    //     if(galleryItem.id == galleryId) {
+    //         galleryItem.likes += 1;
+    //     }
+    // }
+    // res.sendStatus(200);
+
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+
+    const sqlText = `SELECT * FROM "gallery";`
+
+    pool.query(sqlText)
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log(`Error making GET query ${sqlText}`, error);
+        res.sendStatus(500);
+    });
 }); // END GET Route
 
 module.exports = router;
