80a7a82f89a89f26ef7855514e832185df3866e4
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-18 18:44:54 -0500

    Configured pool.js for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,21 +1,34 @@
 const pg = require('pg');
+const url = require('url');
 
-const Pool = pg.Pool;
+let config = {};
 
-const pool = new Pool({
-    database: 'react_gallery',
-    host: 'localhost', 
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
+if (process.env.DATABASE_URL) {
+    config = {
+      // We use the DATABASE_URL from Heroku to connect to our DB
+      connectionString: process.env.DATABASE_URL,
+      // Heroku also requires this special `ssl` config
+      ssl: { rejectUnauthorized: false },
+    };
+} else {
+    config = {
+        database: 'react_gallery',
+        host: 'localhost', 
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000
+    }
+}
+
+const pool = new pg.Pool(config);
 
 pool.on('connect', () => {
     console.log('Connected to the database');
 });
 
-pool.on('error', (error) => {
-    console.log('Error with database pool', error);
-});
+pool.on('error', (err) => {
+    console.log('Unexpected error on idle client', err);
+    process.exit(-1);
+  });
 
 module.exports = pool;
